656b786ca1487a2aee9636563ebf3fa23bc03d73
songbin <songbin.sky@hotmail.com>
2025-06-20 18:11:27 +0800

    布局OK

--- bytecubpdf/src/renderer/components/mainchat/MainChatLeftSide.vue
+++ bytecubpdf/src/renderer/components/mainchat/MainChatLeftSide.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { Conversations } from 'vue-element-plus-x'
 import type { ConversationItem, ConversationMenuCommand } from 'vue-element-plus-x/types/Conversations'
-import { useMessage,NTabs, NTabPane,NButton,NIcon,NFlex,NInput } from 'naive-ui'
-import {AddComment,SearchLocate} from '@vicons/carbon'
+import { useMessage, NTabs, NTabPane, NButton, NIcon, NFlex, NInput, NInputGroup } from 'naive-ui'
+import { AddComment, SearchLocate } from '@vicons/carbon'
 const message = useMessage()
 const menuTestItems = ref([
   {
@@ -64,7 +64,7 @@ const menuTestItems = ref([
     label: '菜单测试项目 14',
   },
 
-    {
+  {
     key: 'm15',
     label: '菜单测试项目 13',
   },
@@ -102,39 +102,37 @@ function handleMenuCommand(command: ConversationMenuCommand, item: ConversationI
   <div>
     <n-tabs type="segment" animated>
       <n-tab-pane name="function" tab="聊天历史">
-         <div style="display: flex; flex-direction: column; gap: 12px; height: calc(100vh - 120px);"> 
-          <Conversations
-            v-model:active="activeKey4"
-            :items="menuTestItems"
-            :label-max-width="160"
-            :show-tooltip="true"
-            row-key="key"
-            tooltip-placement="right"
-            :tooltip-offset="35"
-            show-to-top-btn
-            show-built-in-menu
-            @menu-command="handleMenuCommand"
-          >
-           <template #header>
-             <n-flex vertical>
-              <n-button size="small">
-                <template #icon>
-                  <n-icon>
-                    <AddComment />
-                  </n-icon>
-                </template>
-                新建对话
-              </n-button>
-              <n-input round  type="text" size="small" placeholder="根据会话名搜索">
-                <template #prefix>
-                  <n-icon :component="SearchLocate" />
-                </template>
-              </n-input>
-             </n-flex>
-             
+        <div style="display: flex; flex-direction: column; gap: 12px; height: calc(100vh - 120px);">
+          <Conversations v-model:active="activeKey4" :items="menuTestItems" :label-max-width="160" :show-tooltip="true"
+            row-key="key" tooltip-placement="right" :tooltip-offset="35" show-to-top-btn show-built-in-menu
+            @menu-command="handleMenuCommand">
+            <template #header>
+              <n-flex vertical>
+                <n-button size="small">
+                  <template #icon>
+                    <n-icon>
+                      <AddComment />
+                    </n-icon>
+                  </template>
+                  新建对话
+                </n-button>
+                <n-input-group>
+
+                  <n-input round type="text" size="small" placeholder="根据会话名搜索">
+                    <template #prefix>
+                      <n-icon :component="SearchLocate" />
+                    </template>
+                  </n-input>
+                  <n-button type="primary" size="small">
+                    搜索
+                  </n-button>
+                </n-input-group>
+
+              </n-flex>
+
             </template>
           </Conversations>
-          </div>
+        </div>
       </n-tab-pane>
       <n-tab-pane name="settings" tab="设置">
         设置区域
@@ -143,5 +141,4 @@ function handleMenuCommand(command: ConversationMenuCommand, item: ConversationI
   </div>
 </template>
 
-<style scoped lang="less">
-</style>
+<style scoped lang="less"></style>
