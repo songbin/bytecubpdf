c59602df85aa6de304a8695b27c9a937ddd65b04
songbin <songbin.sky@hotmail.com>
2025-06-21 14:00:39 +0800

    update

--- bytecubpdf/src/renderer/components/mainchat/MainChatContent.vue
+++ bytecubpdf/src/renderer/components/mainchat/MainChatContent.vue
@@ -4,9 +4,9 @@ import type { BubbleListItemProps, BubbleListProps } from 'vue-element-plus-x/ty
 import { BubbleList, MentionSender } from 'vue-element-plus-x'
 import aiAvatar from '@/renderer/assets/avatars/ai-avatar.png'
 import userAvatar from '@/renderer/assets/avatars/user-avatar.png'
-import { NFlex, NButton, NIcon,NSelect,NTag,NButtonGroup,useMessage } from 'naive-ui'
-import { Delete, CopyFile, Edit,SendAlt } from '@vicons/carbon'
-import { Refresh,Attach,Add,TrainOutline as TrainIcon } from '@vicons/ionicons5';
+import { NFlex, NButton, NIcon, NSelect, NTag, NButtonGroup, useMessage, NModal,NCard } from 'naive-ui'
+import { Delete, CopyFile, Edit, SendAlt } from '@vicons/carbon'
+import { Refresh, Attach, Add, TrainOutline as TrainIcon } from '@vicons/ionicons5';
 import { BrainCircuit20Regular } from '@vicons/fluent';
 import { LlmModelManager } from '@/renderer/service/manager/LlmModelManager';
 import MainChatIndexDb from '@/renderer/service/indexdb/MainChatIndexDb';
@@ -20,9 +20,9 @@ const llmManager = new LlmModelManager();
 const mainChatIndexDb = new MainChatIndexDb();
 const formData = ref({
   platformId: '',
-  platformName:'',
+  platformName: '',
   modelId: '',
-  modelName:'',
+  modelName: '',
 })
 const platforms = ref<Array<{ value: string; label: string }>>([]);
 const models = ref<Array<{ value: string; label: string }>>([]);
@@ -36,22 +36,23 @@ const handlePlatformChange = async (platformId: string) => {
   const modelList = await llmManager.getModelsByPlatform(platformId);
   formData.value.platformName = platforms.value.find((p) => p.value == platformId)?.label || '';
   models.value = modelList.map((m) => ({
-      value: m.id,
-      label: m.name,
-    }));
-    if (modelList.length > 0) {
-      formData.value.modelId = modelList[0].id;
-      formData.value.modelName = modelList[0].name;
-    } else {
-      formData.value.modelId = '';
-      formData.value.modelName = ''
-    }
+    value: m.id,
+    label: m.name,
+  }));
+  if (modelList.length > 0) {
+    formData.value.modelId = modelList[0].id;
+    formData.value.modelName = modelList[0].name;
+  } else {
+    formData.value.modelId = '';
+    formData.value.modelName = ''
+  }
 };
 const renderLabel = (option: { label: string }) => {
   return option.label;
 }
-const showSelectModel = () =>{
-  message.success('111')
+const showSelectModel = ref(false)
+const handleShowSelectModel = () => {
+  showSelectModel.value = true;
 }
 onMounted(async () => {
   try {
@@ -61,8 +62,8 @@ onMounted(async () => {
       formData.value = {
         platformId: config.platformId || '',
         modelId: config.modelId || '',
-        platformName:config.platformName || '',
-        modelName:config.modelName || '',
+        platformName: config.platformName || '',
+        modelName: config.modelName || '',
       };
       if (config.platformId) {
         await handlePlatformChange(config.platformId);
@@ -73,27 +74,30 @@ onMounted(async () => {
       value: p.id,
       label: p.platformName,
     }));
-   if(formData.value.platformId == ''){
-     formData.value.platformId = platforms.value[0].value;
-     formData.value.platformName = platforms.value[0].label;
-     handlePlatformChange(formData.value.platformId);
-   }
+    if (formData.value.platformId == '') {
+      formData.value.platformId = platforms.value[0].value;
+      handlePlatformChange(formData.value.platformId);
+    }
+
+    handlePlatformChange(formData.value.platformId);
 
   } catch (error) {
     console.error('加载配置失败:', error);
   }
-   
+
 });
 
 watch(
   formData,
   async (newValue) => {
+    formData.value.platformName = platforms.value.find((p) => p.value == newValue.platformId)?.label || '';
+    formData.value.modelName = models.value.find((m) => m.value == newValue.modelId)?.label || '';
     await mainChatIndexDb.saveConfig({
-      id: 'currentConfig', 
+      id: 'currentConfig',
       platformId: newValue.platformId,
       modelId: newValue.modelId,
-      platformName:newValue.platformName,
-      modelName:newValue.modelName
+      platformName: formData.value.platformName,
+      modelName: formData.value.modelName
     });
   },
   { deep: true }
@@ -136,123 +140,124 @@ function generateFakeItems(count: number): listType[] {
 </script>
 
 <template>
-  <n-flex vertical>
-    <div style="height: calc(100vh - 300px);">
-      <BubbleList :list="list" max-height="100%">
-        <!-- 自定义头部 -->
-        <template #header="{ item }">
-          <div class="header-wrapper">
-            <div class="header-name">
-              {{ item.role === 'ai' ? '小书芽' : '用户' }}
+  <div>
+    <n-flex vertical>
+      <div style="height: calc(100vh - 300px);">
+        <BubbleList :list="list" max-height="100%">
+          <!-- 自定义头部 -->
+          <template #header="{ item }">
+            <div class="header-wrapper">
+              <div class="header-name">
+                {{ item.role === 'ai' ? '小书芽' : '用户' }}
+              </div>
             </div>
-          </div>
-        </template>
-        <template #footer>
-          <n-flex>
-            <n-button tertiary circle type="info">
-              <template #icon>
-                <n-icon>
-                  <Edit />
-                </n-icon>
-              </template>
-            </n-button>
-            <n-button tertiary circle type="info">
-              <template #icon>
-                <n-icon>
-                  <Refresh />
-                </n-icon>
-              </template>
-            </n-button>
-            <n-button tertiary circle type="info">
-              <template #icon>
-                <n-icon>
-                  <CopyFile />
-                </n-icon>
-              </template>
-            </n-button>
-            <n-button tertiary circle type="info">
-              <template #icon>
-                <n-icon>
-                  <Delete />
-                </n-icon>
-              </template>
-            </n-button>
+          </template>
+          <template #footer>
+            <n-flex>
+              <n-button tertiary circle type="info">
+                <template #icon>
+                  <n-icon>
+                    <Edit />
+                  </n-icon>
+                </template>
+              </n-button>
+              <n-button tertiary circle type="info">
+                <template #icon>
+                  <n-icon>
+                    <Refresh />
+                  </n-icon>
+                </template>
+              </n-button>
+              <n-button tertiary circle type="info">
+                <template #icon>
+                  <n-icon>
+                    <CopyFile />
+                  </n-icon>
+                </template>
+              </n-button>
+              <n-button tertiary circle type="info">
+                <template #icon>
+                  <n-icon>
+                    <Delete />
+                  </n-icon>
+                </template>
+              </n-button>
 
-          </n-flex>
-        </template>
-      </BubbleList>
-    </div>
+            </n-flex>
+          </template>
+        </BubbleList>
+      </div>
 
 
-    <div style="display: flex; flex-direction: column; gap: 5px;">
-      <n-flex>
+      <div style="display: flex; flex-direction: column; gap: 5px;">
+        <n-flex>
           <n-button size="tiny">
             PDF翻译
           </n-button>
-           <n-button size="tiny">
+          <n-button size="tiny">
             图片OCR
           </n-button>
-      </n-flex>
-      <MentionSender  v-model="senderValue" variant="updown" :auto-size="{ minRows: 2, maxRows: 4 }" clearable
-        allow-speech placeholder="Enter=发送, SHIFT + ENTER = 换行">
-        <template #prefix>
-          <n-flex>
-              <n-button size="small" tertiary circle type="info">
-              <template #icon>
-                <n-icon>
-                  <Add />
-                </n-icon>
-              </template>
-            </n-button>
-            <n-button-group>
-             
-              <n-button  size='tiny' secondary  @click="showSelectModel()">
-                <template #icon>
-                  <n-icon><TrainIcon/></n-icon>
-                </template>
-                  {{formData.platformName}}|{{ formData.modelName }}
-              </n-button>
-            </n-button-group>
-            
-             <n-select 
-              v-model:value="formData.platformId" 
-              :options="platforms"  
-              filterable 
-              @update:value="handlePlatformChange"
-              menu-size="tiny" 
-              size="tiny"
-              class="adaptive-select"
-              :render-label="renderLabel"
-            />
-            
-            
-            <n-select 
-              v-model:value="formData.modelId" 
-              :options="models"  
-              filterable 
-              menu-size="tiny" 
-              size="tiny"
-              class="adaptive-select"
-              :render-label="renderLabel"
-            />
-          </n-flex>
-        </template>
-
-        <template #action-list>
-          <n-flex  >
-             <n-button size="small" tertiary circle type="primary">
-              <template #icon>
-                <n-icon>
-                  <SendAlt />
-                </n-icon>
-              </template>
-              </n-button>
         </n-flex>
-           
-        </template>
-      </MentionSender>
-    </div>
-  </n-flex>
+        <MentionSender v-model="senderValue" variant="updown" :auto-size="{ minRows: 2, maxRows: 4 }" clearable
+          allow-speech placeholder="Enter=发送, SHIFT + ENTER = 换行">
+          <template #prefix>
+            <n-flex>
+              <n-button size="small" tertiary circle type="info">
+                <template #icon>
+                  <n-icon>
+                    <Add />
+                  </n-icon>
+                </template>
+              </n-button>
+              <n-button-group>
+
+                <n-button size='tiny' secondary @click="handleShowSelectModel()">
+                  <template #icon>
+                    <n-icon>
+                      <TrainIcon />
+                    </n-icon>
+                  </template>
+                  {{ formData.platformName }}|{{ formData.modelName }}
+                </n-button>
+              </n-button-group>
+
+             
+            </n-flex>
+          </template>
+
+          <template #action-list>
+            <n-flex>
+              <n-button size="small" tertiary circle type="primary">
+                <template #icon>
+                  <n-icon>
+                    <SendAlt />
+                  </n-icon>
+                </template>
+              </n-button>
+            </n-flex>
+
+          </template>
+        </MentionSender>
+      </div>
+    </n-flex>
+    <n-modal title="大模型配置"  v-model:show="showSelectModel" preset="card" style="width: 600px;">
+       <n-card >
+          <n-flex vertical>
+                <n-flex>
+                  <n-button text size="small">平台</n-button>
+                  <n-select v-model:value="formData.platformId" :options="platforms" filterable
+                    @update:value="handlePlatformChange"  size="small" class="adaptive-select"
+                    :render-label="renderLabel" />
+                </n-flex>
+                <n-flex>
+                  <n-button text size="small">模型</n-button>
+                  <n-select v-model:value="formData.modelId" :options="models" filterable  size="small"
+                    class="adaptive-select" :render-label="renderLabel" />
+                </n-flex>
+          </n-flex>
+      </n-card>
+    </n-modal>
+  </div>
 </template>
 
 <style scoped>
@@ -263,27 +268,6 @@ function generateFakeItems(count: number): listType[] {
   padding: 3px 12px;
   font-weight: 700;
 }
-/* 新增自适应选择器样式 */
-.adaptive-select {
-  display: inline-block;
-  width: auto !important;
-}
 
-/* 深度选择器修改内部样式 */
-.adaptive-select :deep(.n-base-selection) {
-  padding: 0 4px !important; /* 减少左右内边距 */
-}
 
-.adaptive-select :deep(.n-base-selection__input) {
-  min-width: 0.5rem !important; /* 允许宽度收缩 */
-}
-
-.adaptive-select :deep(.n-base-selection-label) {
-  padding: 0 !important; /* 移除标签内边距 */
-  display: inline-block;
-}
-
-.adaptive-select :deep(.n-base-selection__suffix) {
-  margin-left: 2px !important; /* 调整图标间距 */
-}
 </style>
