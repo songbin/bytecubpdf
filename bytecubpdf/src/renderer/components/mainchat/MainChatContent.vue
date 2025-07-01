798a640925728270eb0f239d0a6e2e26f149a9b7
songbin <songbin.sky@hotmail.com>
2025-07-01 20:54:27 +0800

    update

--- bytecubpdf/src/renderer/components/mainchat/MainChatContent.vue
+++ bytecubpdf/src/renderer/components/mainchat/MainChatContent.vue
@@ -255,10 +255,8 @@ const buildMessageItem = async (role: 'system' | 'user' | 'assistant', content: 
   const reasoning_content = ''//?: string
   const nowTime = new Date().toLocaleString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).replace(/\//g, '-').replace(/\s/, ' ');
   let fileName = ''
-  console.log('role->', role)
   if(role === 'user'){
     fileName = getFileName()
-    console.log('filename->',fileName)
   }
   return {
     key, // 唯一标识
